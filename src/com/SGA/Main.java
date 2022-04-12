fa05ea3e7d72b05c5ff1cc084015238e929b6d49
mercarto95 <79480140+mercarto95@users.noreply.github.com>
2022-04-12 17:57:06 +0200

    Rodriguez

--- src/com/SGA/Main.java
+++ src/com/SGA/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Martin push");
         System.out.println("Rodriguez Push");
+        System.out.println("Hello");
 
     }
 }
