d1394c6f96b8cb60b259cb574f94a9db135d905c
mercarto95 <ahmeedo.khaled@outlook.com>
2022-04-14 23:41:22 +0200

    * test code by Rodriguez ,  * ville bara veta hur kan man på bästa sätet skapar lämpliga klasser  * för projectet * What i have done  * -> Class for competitions & its functions  * -> Class for Athletes & its functions  *-> Class ReadFromFile & its functions  *-> enum for athlete sex  * -> Some tests in Main.  * Förbättringar behöver visst göras vid behöv.  * MVH Rodriguez

--- src/com/SGA/ReadFromFile.java
+++ src/com/SGA/ReadFromFile.java
@@ -5,6 +5,7 @@ package com.SGA;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 /* To be done (by Rodriguez next time)
@@ -15,6 +16,7 @@ import java.util.Scanner;
 * */
 public class ReadFromFile {
 
+    static protected ArrayList<Athlete> athletesMemory = new ArrayList<Athlete>();
 
     static public void readFile(String fileName) {
         String buffer;
@@ -45,6 +47,7 @@ public class ReadFromFile {
 
     // delete the comma "," from each line from the file & extract variables for each atlete & competition.
     static private void extractData(String buffer) {
+        // club, fName, sName, Sex, Age, competitions
         String[] splittedBuffer = new String[50];
         System.out.println("Extracted Line = ");
         splittedBuffer = buffer.split(",");
@@ -52,5 +55,58 @@ public class ReadFromFile {
             System.out.println("-> " + i);
         }
 
+        String clup, firstName, lastName;
+        Sex sex;
+        int age;
+        clup = assignValue(splittedBuffer[0]); // checks the value is empty or not
+        firstName = assignValue(splittedBuffer[1]);
+        lastName = assignValue(splittedBuffer[2]);
+        switch (splittedBuffer[3]){
+            case "F" -> sex = Sex.F;
+            case "f" -> sex = Sex.F;
+            case "M" -> sex = Sex.M;
+            case "m" -> sex = Sex.M;
+            default -> sex = null;
+        }
+        age = assignInt(splittedBuffer[4]);
+
+        System.out.println("clup         : " + clup);
+        System.out.println("first Name   : " + firstName);
+        System.out.println("last Name    : " + lastName);
+        System.out.println("Sex          : " + sex);
+        System.out.println("Age          : " + age);
+
+        // create new athlete & add it to the athlete memory
+        Athlete obj = new Athlete(clup, firstName, lastName, sex, age);
+        athletesMemory.add(obj);
+
+        // needed to be done: 1. extract competitions & create objects & add them to competitions memory
+
+
+
+    }
+
+    static private String assignValue(String elemtnFromBuffer){
+        if( ! elemtnFromBuffer.equals("")){
+            return elemtnFromBuffer;
+        }
+        else
+            return null;
+    }
+
+    static private int assignInt(String elemtnFromBuffer){
+        if( ! elemtnFromBuffer.equals("")){
+            return Integer.parseInt( elemtnFromBuffer );
+        }
+        else
+            return -555; // means that the fild is empty
+    }
+
+    static private float assignfloat(String elemtnFromBuffer){
+        if( ! elemtnFromBuffer.equals("")){
+            return Float.parseFloat( elemtnFromBuffer );
+        }
+        else
+            return -555; // means that the fild is empty
     }
 }
