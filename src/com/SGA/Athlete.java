8b516657abd478b2893d5d731aeb7a429f530bf8
FREEDOM <vinxad@gmail.com>
2022-05-05 17:25:12 +0200

    Fixed athlete constructor Able to instantiate athletes Added hashmap with all scores Method to convert time, yada yada

--- src/com/SGA/Athlete.java
+++ src/com/SGA/Athlete.java
@@ -1,11 +1,14 @@
 package com.SGA;
 
+import java.util.HashMap;
+
 public class Athlete {
     private String firstName;
     private String lastName;
     private int age;
 
     private SexCategory sex;
+    /*
     private double running60;
     private double running200;
     private double running800;
@@ -17,6 +20,8 @@ public class Athlete {
     private double jumpingHigh;
     private double jumpingPole;
     private double throwingShot;
+    */
+    private HashMap<CompetitionType, Double> competitionResults;
 
 
     // Getters and setters
@@ -44,9 +49,14 @@ public class Athlete {
         this.age = age;
     }
 
-    // TODO: Finish this
+    // Return hashmap
+    public HashMap<CompetitionType, Double> getCompetitionResults() {
+        return competitionResults;
+    }
+
+    // TODO: Add more checks to see if input is valid
     Athlete(String ath) {
-        // split string with delimiter ","
+        // Split string with delimiter ","
         String[] athlete = ath.split(",");
 
         // Validate input if length is 16
@@ -57,20 +67,64 @@ public class Athlete {
             else this.sex = SexCategory.Female;
             this.age = Integer.parseInt(athlete[4]);
 
-            int result = athlete[7].indexOf(':');
+            // Setup hashmap
+            competitionResults = new HashMap<>();
 
-            if (result != -1) {
-                this.running60 = Double.parseDouble(athlete[7].substring(result+1, athlete[7].length()));
-                System.out.println(this.running60);
-            }
-            else if (athlete[7].equals("")) {
-               // this.running60 = Double.parseDouble(athlete[5]);
-                System.out.println("No result for 60m");
-            }
-            else {
-            }
+            competitionResults.put(CompetitionType.RUNNING60, convertTime(athlete[5]));
+            competitionResults.put(CompetitionType.RUNNING200, convertTime(athlete[6]));
+            competitionResults.put(CompetitionType.RUNNING800, convertTime(athlete[7]));
+            competitionResults.put(CompetitionType.RUNNING1500, convertTime(athlete[8]));
+            competitionResults.put(CompetitionType.RUNNING3000, convertTime(athlete[9]));
+            competitionResults.put(CompetitionType.HURDLES, convertTime(athlete[10]));
+            competitionResults.put(CompetitionType.LONGJUMPING, convertTime(athlete[11]));
+            competitionResults.put(CompetitionType.TRIPLEJUMPING, convertTime(athlete[12]));
+            competitionResults.put(CompetitionType.HIGHJUMPING, convertTime(athlete[13]));
+            competitionResults.put(CompetitionType.POLEJUMPING, convertTime(athlete[14]));
+            competitionResults.put(CompetitionType.THROWING, convertTime(athlete[15]));
 
+            //System.out.println(competitionResults.get(CompetitionType.RUNNING60));
+            //System.out.println(competitionResults.get(CompetitionType.THROWING));
         }
 
+    }
+
+    // Converts input string time to seconds.milliseconds
+    // e.g "00:01.000" to 1
+    // e.g "01:01.001" to 61.001
+    // returns -1 if input is invalid or non-existent
+    private double convertTime(String time) {
+        double secondsResult;
+
+        // Get ":" index and process string if it does exist
+        int result = time.indexOf(':');
+        if (result != -1) {
+            /*
+             * e.g 03:05.123 -> 3*60 + 5.123
+             * First half of the string is the minutes
+             * Second half of the string is the seconds.milliseconds
+             */
+            secondsResult = parseDouble(time.substring(result + 1));
+            secondsResult += 60 * parseDouble(time.substring(0, result));
+        }
+        // Else just parse time instantly, if time doesnt exist, return -1
+        else {
+            secondsResult = parseDouble(time);
+        }
+
+        return secondsResult;
+    }
+
+    // Parse a string to double
+    // Returns -1 if input is invalid or non-existent
+    private double parseDouble(String str) {
+        double result;
+        try {
+            result = Double.parseDouble(str);
+        } catch (NumberFormatException e) {
+            //System.err.println("Invalid input: " + e.getMessage() + ", returning -1");
+            //System.err.println("Input: " + str);
+            result = -1;
+        }
+        return result;
     }
 }
