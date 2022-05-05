0aac9267a9d5e4b264e7f264b70a6565850ef974
Tempron <43298787+Tempron@users.noreply.github.com>
2022-05-05 09:21:36 +0200

    Sex category

--- src/com/SGA/Scheduler.java
+++ src/com/SGA/Scheduler.java
@@ -1,6 +1,8 @@
 package com.SGA;
 
 import java.io.File;
+import java.util.HashMap;
+import java.util.Vector;
 
 public class Scheduler {
     private String participantsRaw;
@@ -22,6 +24,21 @@ public class Scheduler {
         athletes = new Athlete[participants.length];
         for (int i = 0; i < participants.length; i++) {
             athletes[i] = new Athlete(participants[i]);
+        }
+    }
+
+    private void fillCompetitions() {
+
+        HashMap<AgeGroup, HashMap<SexCategory, Competition>> competitions = new HashMap<>();
+
+        HashMap<SexCategory, Competition> tmp = new HashMap<>();
+        tmp.put(SexCategory.Male, new PoleJumping());
+        competitions.put(AgeGroup.age9_10, tmp);
+
+        for (HashMap<SexCategory, Competition> i: competitions.values()) {
+            for (Competition j : i.values()) {
+                j.getNext();
+            }
         }
     }
 
---
+++ src/com/SGA/SexCategory.java
@@ -0,0 +1,7 @@
+package com.SGA;
+
+public enum SexCategory {
+    Male,
+    Female,
+    Both
+}
