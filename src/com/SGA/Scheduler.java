f1f05383f80c04240df0e1a38e14fbf07298a51f
FREEDOM <vinxad@gmail.com>
2022-05-06 15:31:53 +0200

    refactoring reduced redundancy

--- src/com/SGA/Scheduler.java
+++ src/com/SGA/Scheduler.java
@@ -5,23 +5,24 @@ import java.util.HashMap;
 import java.util.Vector;
 
 public class Scheduler {
-    private String participantsRaw;
     HashMap<AgeGroup, HashMap<SexCategory, Competition>> competitions = new HashMap<>();
-    Athlete[] athletes;
+    private Athlete[] athletes;
 
     // Constructor
     public Scheduler() {
     }
 
     // Generate schedule
-    public void generateSchedule(String inputCsv) {
-        loadParticipants(inputCsv);
-        initializeAthletes();
+    public void generateSchedule(String filePath) {
+        initializeAthletes(filePath);
     }
 
-    private void initializeAthletes() {
-        // Loop through participants raw data
-        String[] participants = participantsRaw.split("\n");
+    private void initializeAthletes(String filePath) {
+        // Load participants raw data and split it into lines
+        String participantsRawData = loadParticipants(filePath);
+        String[] participants = participantsRawData.split("\n");
+
+        // Create athletes array and fill it with participants
         athletes = new Athlete[participants.length];
         for (int i = 0; i < participants.length; i++) {
             athletes[i] = new Athlete(participants[i]);
@@ -44,10 +45,10 @@ public class Scheduler {
     }
 
     // Load participants data from csv file
-    private void loadParticipants(String filePath) {
+    private String loadParticipants(String filePath) {
         FileHandler fileHandler = new FileHandler();
         fileHandler.readFileFromPath(filePath);
-        participantsRaw = fileHandler.getFileContent();
+        return fileHandler.getFileContent();
     }
 
 
