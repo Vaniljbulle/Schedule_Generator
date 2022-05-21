6f8646eb0e55e9fb6a852a4b8ab779fa4b3c030f
FREEDOM <vinxad@gmail.com>
2022-05-21 23:58:31 +0200

    cleanup

--- src/com/SGA/Main.java
+++ src/com/SGA/Main.java
@@ -1,13 +1,8 @@
 package com.SGA;
 
-import java.util.Vector;
-
 public class Main {
-
     public static void main(String[] args) {
-
         Scheduler scheduler = new Scheduler();
         scheduler.generateSchedule("registration-list.csv", "out.csv");
     }
-
 }
