20238ebb577bc5d6cc8294502d6c612b1c06f29d
FREEDOM <vinxad@gmail.com>
2022-05-22 17:18:58 +0200

    restored api for testing

--- src/com/SGA/Main.java
+++ src/com/SGA/Main.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scheduler scheduler = new Scheduler();
+        /*
         // API Input and output
         Scanner input = new Scanner(System.in);
         System.out.println("\n\n\n======================= SGA SCHEDULE GENERATOR =======================");
@@ -14,7 +15,7 @@ public class Main {
         System.out.println("Please, enter the name of the output file: ");
         String destFile = input.nextLine();
         System.out.println("\n======================================================================\n\n\n");
-
-        scheduler.generateSchedule(fileName + ".csv", destFile + ".csv");
+        */
+        scheduler.generateSchedule("registration-list.csv", "out.csv");
     }
 }
